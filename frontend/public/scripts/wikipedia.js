97b28a53ae1feb15ba94d890b7cdb8c5e858696c
“Argeo <118343827+Arg022@users.noreply.github.com>
2024-05-24 14:57:34 +0200

    wikiperdia

--- frontend/public/scripts/wikipedia.js
+++ frontend/public/scripts/wikipedia.js
@@ -4,6 +4,12 @@ import fs from 'fs';
 
 const turndownService = new TurndownService();
 
+async function searchWikipedia(searchTerm) {
+  const response = await fetch(`https://it.wikipedia.org/w/api.php?action=query&list=search&srsearch=${searchTerm}&utf8=&format=json`);
+  const data = await response.json();
+  return data.query.search.map(result => result.title);
+}
+
 async function wikipediaToMarkdown(pageTitle) {
   const response = await fetch(`https://it.wikipedia.org/w/api.php?action=parse&page=${pageTitle}&format=json`);
   const data = await response.json();
@@ -12,11 +18,6 @@ async function wikipediaToMarkdown(pageTitle) {
   return markdownContent;
 }
 
-async function searchWikipedia(searchTerm) {
-  const response = await fetch(`https://it.wikipedia.org/w/api.php?action=query&list=search&srsearch=${searchTerm}&utf8=&format=json`);
-  const data = await response.json();
-  return data.query.search.map(result => result.title);
-}
 
 searchWikipedia('JavaScript').then(console.log);
 wikipediaToMarkdown('JavaScript').then(markdownContent => {
