e9380c1b67044e6760aad9135f03808924ea1b35
Argeo Prosperi <argeoprosperi22@gmail.com>
2024-05-27 12:12:24 +0200

    prova cambio da simpleMDE ad editor.js

--- frontend/public/scripts/wikipedia.js
+++ frontend/public/scripts/wikipedia.js
@@ -12,6 +12,14 @@ async function searchWikipedia(searchTerm) {
     return data.query.search.map((result) => result.title);
 }
 
+async function searchRandom(searchTerm) {
+    const response = await fetch(
+        `https://en.wikipedia.org/wiki/Special:Random`
+    );
+    const data = await response.json();
+    return data.query.search.map((result) => result.title);
+}
+
 async function wikipediaToMarkdown(pageTitle) {
     const response = await fetch(
         `https://it.wikipedia.org/w/api.php?action=parse&page=${pageTitle}&format=json`
