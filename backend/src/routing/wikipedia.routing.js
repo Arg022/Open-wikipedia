09b3382607a9f7fdfca454bc1dd5fafec1fd2828
“Argeo <118343827+Arg022@users.noreply.github.com>
2024-05-31 14:50:47 +0200

    commuit routing wikipedia

--- backend/src/routing/wikipedia.routing.js
+++ backend/src/routing/wikipedia.routing.js
@@ -4,20 +4,24 @@ import {
 } from "../services/wikipedia.service.js";
 import prisma from "../../db/prisma.js";
 import fs from "fs";
+import path from "path";
+import { fileURLToPath } from 'url'; // Importa fileURLToPath e url
 
+const __filename = fileURLToPath(import.meta.url); // Ottieni il nome del file corrente
+const __dirname = path.dirname(__filename); // Ottieni la directory del file corrente
 
 export default function wikipediaRouting(app) {
     app.post("/search", async (req, res) => {
-        const searchTerm = req.body.query;
+        const searchTerm = req.body.q;
         if (!searchTerm) {
-            return res.status(400).json({ error: "Query is required" });
+            return res.status(400).json({ error: "Search term is required" });
         }
+
         try {
             const results = await searchWikipedia(searchTerm);
             res.json(results);
         } catch (error) {
-            console.error("Error searching Wikipedia:", error);
-            res.status(500).json({ error: "Failed to search Wikipedia" });
+            res.status(500).json({ error: "Error searching Wikipedia" });
         }
     });
 
@@ -31,18 +35,18 @@ export default function wikipediaRouting(app) {
             const markdownContent = await wikipediaToMarkdown(title);
 
             const filePath = path.join(__dirname, "../content", `${title}.md`);
-
             fs.writeFileSync(filePath, markdownContent, "utf8");
 
             const newPage = await prisma.wikipediaPage.create({
                 data: {
                     title: title,
-                    filePath: filePath,
+                    filePath: filePath
                 },
             });
 
             res.status(201).json(newPage);
         } catch (error) {
+            console.error("Failed to save article to the database:", error);
             res.status(500).json({ error: "Failed to save article" });
         }
     });
